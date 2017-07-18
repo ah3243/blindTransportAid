ec69cef02c588a65088fddbecfb0ed86afe27418
ah3243 <alberthiggins@alberts-MacBook-Pro.local>
2017-07-18 20:11:36 +0100

    central width control, show fps, handle keyboard interrupt

--- 6_buttonVideoSave/btnVideoSave.py
+++ 6_buttonVideoSave/btnVideoSave.py
@@ -6,6 +6,8 @@ import cv2
 import imutils
 import time
 from imutils.video import VideoStream
+from imutils.video import FPS
+
 
 # # Button imports
 import RPi.GPIO as GPIO
@@ -21,106 +23,132 @@ GPIO.setup(GPIO_StartBtn, GPIO.IN, pull_up_down=GPIO.PUD_UP) # start recording o
 GPIO.setup(GPIO_LED, GPIO.OUT) # LED to show recording
 #GPIO.setup(GPIO_VibrationMotor, GPIO.OUT) # PWM for vibration motor
 
+imgWidth = 800
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-o", "--output", required=False,
-	help="path to output video file")
+    help="path to output video file")
 ap.add_argument("-p", "--picamera", type=int, default=1,
-	help="whether or not the Raspberry Pi camera should be used")
+    help="whether or not the Raspberry Pi camera should be used")
 ap.add_argument("-d", "--display", type=int, default=-1, help="Add -d 1 to display the captured images")
 args = vars(ap.parse_args())
 
 if args["display"] == 1:
-	print("video display activated ")
-	cv2.namedWindow("Frame")
+    print("video display activated ")
+    cv2.namedWindow("Frame")
 
-# for c in range(1):
-while(True):
-	input_state = GPIO.input(GPIO_StartBtn)
-	if input_state == False:
-		print('Button Pressed')
 
-		GPIO.output(GPIO_LED, GPIO.HIGH)
+cnt= 1 # to initialise fps at start of button press
 
-		# initialize the video stream and allow the camera
-		# sensor to warmup
-		print("[INFO] warming up camera...")
-		vs = VideoStream(usePiCamera=1).start()
-		time.sleep(2.0)
+try:
+    while(True):
+        input_state = GPIO.input(GPIO_StartBtn)
 
-		# initialize the FourCC, video writer, dimensions of the frame, and
-		# zeros array
-		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-		writer = None
-		(h, w) = (None, None)
-		zeros = None
+        # start loop if button pressed
+        if input_state:
+            print('Button Pressed')
 
-		rootName = "TestVid"
-		# curTime = '_'+ str(Tme.hour()) + '_' + str(Tme.minute()) + '_' + str(Tme.second())
-		Tme = Tme.now()
-		curTime = "_" + str(Tme.hour) + "-" + str(Tme.minute)
-		# print("Hours: {}, Minutes: {}, Seconds: {}".format(Tme.hour(), Tme.minute(), Tme.second()))
-		print("hours: {}, minutes: {}".format(Tme.now().hour, Tme.now().minute))
+            # initialise fps at start of video loop
+            if cnt:
+                fps = FPS().start()
+                cnt =0
 
-		extension = ".avi"
+            GPIO.output(GPIO_LED, GPIO.HIGH)
 
-		vidName = rootName + curTime + extension
-		print("This is the time now: {}\n This is the vidName: {}".format(curTime, vidName))
+            # initialize the video stream and allow the camera
+            # sensor to warmup
+            print("[INFO] warming up camera...")
+            vs = VideoStream(usePiCamera=1).start()
+            time.sleep(2.0)
 
-		defaultFps = 25
-	
-		# store the image dimensions, initialzie the video writer,
-		# and construct the zeros array
-		frame = vs.read()
-		frame = imutils.resize(frame, width=300)
-		(h, w) = frame.shape[:2]
+            # initialize the FourCC, video writer, dimensions of the frame, and
+            # zeros array
+            fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+            writer = None
+            (h, w) = (None, None)
+            zeros = None
 
-		writer = cv2.VideoWriter(vidName, fourcc, defaultFps,
-			(w, h), True)
+            rootName = "TestVid"
+            # curTime = '_'+ str(Tme.hour()) + '_' + str(Tme.minute()) + '_' + str(Tme.second())
+            Tme = Tme.now()
+            curTime = "_" + str(Tme.hour) + "-" + str(Tme.minute)
+            # print("Hours: {}, Minutes: {}, Seconds: {}".format(Tme.hour(), Tme.minute(), Tme.second()))
+            print("hours: {}, minutes: {}".format(Tme.now().hour, Tme.now().minute))
 
-		## loop over frames from the video stream
-		# num_frames = 100
-		# for i in range(num_frames):
-		while(True):
+            extension = ".avi"
 
-			# grab the frame from the video stream and resize it to have a
-			# maximum width of 300 pixels
-			frame = vs.read()
-			frame = imutils.resize(frame, width=300)
-		
-			writer.write(frame)
-			print("Frame size: {}, shape: {}".format(frame.size, frame.shape))
+            vidName = rootName + curTime + extension
+            print("This is the time now: {}\n This is the vidName: {}".format(curTime, vidName))
 
-			if args["display"] == 1:
-				# show the frames
-				cv2.imshow("Frame", frame)
+            defaultFps = 25
+        
+            # store the image dimensions, initialzie the video writer,
+            # and construct the zeros array
+            frame = vs.read()
+            frame = imutils.resize(frame, width=imgWidth)
+            (h, w) = frame.shape[:2]
 
-			# cv2.imshow("Output", output)
-			key = cv2.waitKey(1) & 0xFF
-		
-			# if the `q` key was pressed, break from the loop
-			if key == ord("q"):
-				break
+            writer = cv2.VideoWriter(vidName, fourcc, defaultFps,
+                (w, h), True)
 
-			input_state = GPIO.input(GPIO_StartBtn)
-			if input_state == True:
-				print('Button unPressed')
-				break
-		
-		# do a bit of cleanup
-		print("[INFO] saving video...")
-		vs.stop()
-		writer.release()
-		cv2.destroyAllWindows()
-		cv2.waitKey(2000)
-	
-	GPIO.output(GPIO_LED, GPIO.LOW)
+            ## loop over frames from the video stream
+            # num_frames = 100
+            # for i in range(num_frames):
+            while(True):
 
-	key = cv2.waitKey(1) & 0xFF
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+                # grab the frame from the video stream and resize it to have a
+                # maximum width of 300 pixels
+                frame = vs.read()
+                frame = imutils.resize(frame, width=imgWidth)
+            
+                writer.write(frame)
+                print("Frame size: {}, shape: {}".format(frame.size, frame.shape))
 
-print("Cleaning up GPIO pins and closing.")
-GPIO.cleanup()
+                if args["display"] == 1:
+                    # show the frames
+                    cv2.imshow("Frame", frame)
+
+                # cv2.imshow("Output", output)
+                key = cv2.waitKey(1) & 0xFF # give it time to process
+            
+                # # if the `q` key was pressed, break from the loop
+                # if key == ord("q"):
+                # 	break
+
+                input_state = GPIO.input(GPIO_StartBtn)
+                if input_state == False:
+                    print('Button unPressed')
+                    break
+                
+                # update the FPS counter
+                fps.update()
+
+            # do a bit of cleanup
+            print("[INFO] saving video...")
+            vs.stop()
+            writer.release()
+            cv2.destroyAllWindows()
+
+            fps.stop() # calculate fps values
+            # display fps values
+            print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+            print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+            time.sleep(2) # wait for 2 seconds for the video to save
+
+
+        GPIO.output(GPIO_LED, GPIO.LOW)
+
+        key = cv2.waitKey(1) & 0xFF
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+    print("Keyboard interupt. Cleaning up GPIO pins and closing.")
+    GPIO.cleanup()
+
+    fps.stop()         # calculate fps values
+    # display fps values
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
