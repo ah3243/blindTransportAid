736e4403daadd0c816826b7119cfa71ccafd9782
ah3243 <alberthiggins@alberts-MacBook-Pro.local>
2017-08-04 21:57:40 +0100

    initial commit

--- 98_DecodeKey/speakText.py
+++ 98_DecodeKey/speakText.py
@@ -1,1 +1,7 @@
 ## this is a class which intakes text and is able to transform it into synthesised speach
+
+import os
+def speakCurrent(inputSpeech):
+    text = 'flite -t "' + inputSpeech + '"'
+    print("This is the text: {}".format(text)) 
+    os.system(text)
