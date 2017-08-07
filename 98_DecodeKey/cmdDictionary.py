5f9749ce129342f847f68fb27b4f6bb2ded7fea7
ah3243 <albert.higgins1@gmail.com>
2017-08-07 17:29:45 +0100

    write brief class to handle/import the current command dictionary

--- 98_DecodeKey/cmdDictionary.py
+++ 98_DecodeKey/cmdDictionary.py
@@ -1,8 +1,38 @@
 ## Define keys:value dictionary
-codeBook = {
-    0: "Station Entrance",
-    1: "ticket stile"
-}
+import os
+import json
+
+class cmdDictionary:
+
+    codeBook = {}
+
+    loc = "../savedDict.json"
+
+    def __init__(self):
+        # import the current command dictionary
+        self.codeBook = self.update()
+
+
+    def update(self):
+        """Imports the current list of combinations and messages"""
+        # check that dict file exists
+        if os.path.isfile(self.loc):
+            try: 
+                with open(self.loc, 'r') as fp:
+                    remoteDict = json.load(fp)
+                # print("Remote Dict opened and imported:\n{}".format(remoteDict))
+                return remoteDict
+        
+            except:
+                print("This is not json loadable")
+        else:
+            print("Currently no remote Dict, will generate on exit")
+    
+        return
+
+    def retnDict(self):
+        """Return current local dictionary"""
+        return self.codeBook
 
 ## Create a dictionary class which has the stored values 
 # and methods to convert indexes into the corresponding strings
